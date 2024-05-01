5ce590ffd559f229709d955d3422c9e90dc8d7a4
易健豪 <yijianhao@vip.qq.com>
2024-05-02 02:55:17 +0800

    fix: 修复登录页面显示不正确以及不会自动关闭登录页的问题

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,10 +1,23 @@
 <script setup>
 
 import Login from "./login";
+import Taro from "@tarojs/taro";
+import {infoToast} from "../../utils/showToast";
+const onAfterLogin = (isSucceed) => {
+  if (isSucceed) {
+    Taro.navigateBack({
+      complete: () => {
+        infoToast("登录成功")
+      }
+    })
+  }
+}
 </script>
 
 <template>
-  <login></login>
+  <div class="h-screen">
+    <login @afterLogin="onAfterLogin"></login>
+  </div>
 </template>
 
 <style scoped>
